3d2c99b0ae741320aa330a0332b713a6fc53ad32
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2025-02-16 02:37:38 +0200

    update swagger link

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ export const setupServer = () => {
   app.use('/water', waterRouter);
   app.use('/user', userRouter);
   app.use('/auth', authRouter);
-  app.use('/api-docs', swaggerDocs());
+  app.use('/c', swaggerDocs());
 
   app.use(notFoundHandler);
 
